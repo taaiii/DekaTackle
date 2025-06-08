737d0260ff2a91547923b5ad0393e819ae34fa25
mikan <2410048@i-seifu.jp>
2025-06-08 10:57:36 +0900

    3人作業のために整えたデータ

---
+++ Assets/Taiyo/Script/Enemy/EnemyManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    //敵プレハブ
+    GameObject silentEnemyL;
+    GameObject silentEnemyR;
+
+    GameObject walkEnemyL;
+    GameObject walkEnemyR;
+
+    GameObject dushEnemyL;
+    GameObject dushEnemyR;
+
+    GameObject truckEnemyL;
+    GameObject truckEnemyR;
+
+    //左右ランダム+敵の種類ランダムでInstabtiate関数を使用して敵を出現させてください。
+    //上が完成したら、一定時間で敵を作成できるように。一定時間はだんだん短くなります
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
