7a393f63c04c36cb2e92abe9d69805510d05d6f9
mikan <2410048@i-seifu.jp>
2025-06-08 14:43:24 +0900

    マネージャー制作（ずっつ左から出てくるバグあり）

--- Assets/Taiyo/Script/Enemy/EnemyManager.cs
+++ Assets/Taiyo/Script/Enemy/EnemyManager.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     //敵プレハブ
-    GameObject silentEnemyL;
-    GameObject silentEnemyR;
+    public GameObject silentEnemyL;
+    public GameObject silentEnemyR;
 
-    GameObject walkEnemyL;
-    GameObject walkEnemyR;
+    public GameObject walkEnemyL;
+    public GameObject walkEnemyR;
 
-    GameObject dushEnemyL;
-    GameObject dushEnemyR;
+    public GameObject dushEnemyL;
+    public GameObject dushEnemyR;
 
-    GameObject truckEnemyL;
-    GameObject truckEnemyR;
+    public GameObject truckEnemyL;
+    public GameObject truckEnemyR;
+
+    public float SpawnInterval = 5f;
+
+    private float timer;
+    private int ramdomEnemyNum;    //敵のランダム情報を保存 
+    private int isRightDirection; //方向のランダム情報を保存
 
     //左右ランダム+敵の種類ランダムでInstabtiate関数を使用して敵を出現させてください。
     //上が完成したら、一定時間で敵を作成できるように。一定時間はだんだん短くなります
@@ -23,12 +29,87 @@ public class EnemyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > SpawnInterval)
+        {
+            timer = 0f;
+
+            //敵と方向をランダムで設定
+            ramdomEnemyNum = RamdomEnemy();
+            isRightDirection = RandomDirection();
+
+            //変数によって作る敵を変更
+            switch (ramdomEnemyNum)
+            {
+                case 0: CreateSilent(); break; //忍び足
+                case 1: CreateWalk();   break; //歩き
+                case 2: CreateDush();   break; //走り　
+            }
+
+        }
+    }
+
+    int RamdomEnemy()
+    {
+        //  0 : 忍び足, 1 : 歩き, 2 : 走り
+        return Random.Range(0, 3);
+    }
+    int RandomDirection()
+    {
+        // 0 : 左, 1 : 右
+        return Random.Range(0, 2);
+    }
+    void CreateSilent()
+    {
+        //右側に作成
+        if (isRightDirection == 0)
+        {
+            Debug.Log("忍び足 : 左");
+            Instantiate(silentEnemyL);
+        }
+        //左側に作成
+        else
+        {
+            Debug.Log("忍び足 : 右");
+            Instantiate(silentEnemyR);
+        }
+    }
+    void CreateWalk()
+    {
+        //右側に作成
+        if (isRightDirection == 0)
+        {
+            Debug.Log("歩き : 左");
+            Instantiate(silentEnemyL);
+        }
+        //左側に作成
+        else
+        {
+            Debug.Log("歩き : 右");
+            Instantiate(silentEnemyR);
+        }
+    }
+    void CreateDush()
+    {
+        //右側に作成
+        if (isRightDirection == 0)
+        {
+            Debug.Log("走り : 左");
+            Instantiate(silentEnemyL);
+        }
+        //左側に作成
+        else
+        {
+            Debug.Log("走り : 右");
+            Instantiate(silentEnemyR);
+        }
+
     }
 }
