00f2793d309980e647648f7bbd97be48c26721fc
okayu2345 <yusuke.oka.620@icloud.com>
2025-07-07 22:42:41 +0900

    リザルト画面でスコアによって結果が変化するようにしました

--- Assets/Taiyo/Script/function/Counttext.cs
+++ Assets/Taiyo/Script/function/Counttext.cs
@@ -1,24 +1,74 @@
-using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
 using TMPro;
+using UnityEngine;
 
 public class Counttext : MonoBehaviour
 {
     public TextMeshProUGUI textMeshProUGUI; // TextMeshProオブジェクトへの参照
-     // カウンター
+                                            // カウンター
+    public GameObject RainbowScore;
+    public GameObject GoldScore;
+    public GameObject SilverScore;
+    public GameObject BronzeScore;
+    public int RainbowPoint;
+    public int GoldPoint;
+    public int SilverPoint;
+    public int BronzePoint;
+    private int minScore;
+
+
 
     void Start()
     {
-        UpdateText(); // 初期表示
+        minScore = 0;
+        //UpdateText(); // 初期表示
+        UpdateText();
+        ScoreEvaluation();
     }
 
     void Update()
     {
+        //UpdateText();
         UpdateText();
+        ScoreEvaluation();
     }
 
     // テキスト更新用のメソッド
     void UpdateText()
     {
         textMeshProUGUI.text = /*"Point: " + */PointCounter.Instance.Point.ToString();
+    }
+
+    private int ScoreEvaluation()
+    {
+        int currentPoint = PointCounter.Instance.Point;
+        if (currentPoint <= RainbowPoint && currentPoint>GoldPoint)
+        {
+            RainbowScore.SetActive(true);
+            UpdateText(); 
+            return 1;
+        }
+        if (currentPoint <= GoldPoint && currentPoint>SilverPoint)
+        {
+            GoldScore.SetActive(true);
+            UpdateText(); 
+            return 2;
+        }
+        if (currentPoint <= SilverPoint && currentPoint > BronzePoint)
+        {
+            SilverScore.SetActive(true);
+            UpdateText(); 
+            return 3;
+        }
+        if (currentPoint <= BronzePoint && currentPoint > minScore)
+        {
+            BronzeScore.SetActive(true);
+            UpdateText(); 
+            return 4;
+        }else
+        {
+            Debug.Log("エラーですよ");
+            return 0;
+        }
     }
 }
